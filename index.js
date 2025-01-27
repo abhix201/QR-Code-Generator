27981256526a4076a55d8dd9b0489abeb0241350
Abhijeet Gupta <abhix201@gmail.com>
2025-01-27 16:07:40 +0530

    changes in the backend code which will take special chars in it

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 function handleSubmit(event) {
 	event.preventDefault();
+	
+	// Get the input value and encode it
 	let text = document.getElementById("inptxt").value;
-	let qrsrc = `https://api.qrserver.com/v1/create-qr-code/?data=${text}`;
+	let encodedText = encodeURIComponent(text);
+
+	// Generate the QR code source URL
+	let qrsrc = `https://api.qrserver.com/v1/create-qr-code/?data=${encodedText}`;
 	let qrimg = document.getElementById("qrimg");
 	qrimg.src = qrsrc;
-  
+
 	// Update the href attribute of the download link
 	let downloadLink = document.getElementById("downloadLink");
 	downloadLink.href = qrsrc;
-  
+
 	// Show the download button
 	downloadLink.style.display = "block";
-  }
-
-  
-  
-  
-  
-  
-  
+}
